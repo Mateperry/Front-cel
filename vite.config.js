dce8521e0cd8eb51b1ef55e6127f4d8b96d1df1a
Mateo <castromateo2004@gmail.com>
2025-03-05 16:37:32 -0500

    Actualización del proyecto para que se actualice bien

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Solo si estás desplegando en GitHub Pages, de lo contrario, usa '/'
-  server: {
-    fs: {
-      strict: false, // Permite servir archivos fuera de la raíz
-    }
-  },
-  build: {
-    assetsInlineLimit: 0, // Para evitar problemas con la carga de archivos estáticos
-  }
+  
 })
